0eee7c9cfdda0489addf96517a1055a1bfca25ac
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2019-11-27 15:02:20 +0530

    working for date query for valid input
    What changes you have made?
    added date query
    added date and empId query
    
    Why this change is necessary?
    to access by date
    
    What is the impact of the change?
    will be able to access by date
    will be able to access by date and empId

--- src/parseCommand.js
+++ src/parseCommand.js
@@ -1,27 +1,26 @@
 const parseSaveCmd = function(userInputs, date) {
     //const saveOptions = ['--beverage','--empId','--qty']
+    const empId = +userInputs[userInputs.lastIndexOf("--empId") + 1];
+    const beverage = userInputs[userInputs.lastIndexOf("--beverage") + 1];
+    const qty = +userInputs[userInputs.lastIndexOf("--qty") + 1];
     const saveCmd = {
         command: "save",
         isValid: true,
-        value: {
-            empId: +userInputs[3],
-            beverage: userInputs[1],
-            qty: +userInputs[5],
-            date
-        }
+        value: { empId, beverage, qty, date }
     };
     //saveCmd.isValid = saveCmd.isValid &&
     return saveCmd;
 };
 
 const parseQueryCmd = function(userInputs) {
-    const queryOptions = ["--empId"];
+    //const queryOptions = ["--empId"];
+    const empId = (userInputs.includes("--empId") && +userInputs[userInputs.lastIndexOf("--empId") + 1]) || undefined;
+    const date = (userInputs.includes("--date") && userInputs[userInputs.lastIndexOf("--date") + 1]) || undefined;
+
     const queryCmd = {
         command: "query",
         isValid: true,
-        value: {
-            empId: +userInputs[1]
-        }
+        value: { empId, date }
     };
     //queryCmd.isValid = queryCmd.isValid && queryOptions.includes(userInputs[0]);
     return queryCmd;
--- test/testParseCommand.js
+++ test/testParseCommand.js
@@ -3,30 +3,43 @@ const parseCommand = require("../src/parseCommand").parseCommand;
 
 describe("Testing parseCommand", function() {
     it("should give parse command for save", function() {
-        const userArgs = ["--save", "--beverge", "orange", "--empId", "25275", "--qty", "1"];
+        const userArgs = ["--save", "--beverage", "orange", "--empId", "25275", "--qty", "1"];
         const date = new Date().toJSON();
 
         const expectedValue = {
             command: "save",
             isValid: true,
-            value: {
-                empId: 25275,
-                beverage: "orange",
-                qty: 1,
-                date
-            }
+            value: { empId: 25275, beverage: "orange", qty: 1, date }
         };
         assert.deepStrictEqual(parseCommand(userArgs, date), expectedValue);
     });
 
-    it("should give parse command for query", function() {
+    it("should give parse command for empId query", function() {
         const userArgs = ["--query", "--empId", "25275"];
         const expectedValue = {
             command: "query",
             isValid: true,
-            value: {
-                empId: 25275
-            }
+            value: { empId: 25275, date: undefined }
+        };
+        assert.deepStrictEqual(parseCommand(userArgs), expectedValue);
+    });
+
+    it("should give parse command for date query", function() {
+        const userArgs = ["--query", "--date", "2019-11-20"];
+        const expectedValue = {
+            command: "query",
+            isValid: true,
+            value: { empId: undefined, date: "2019-11-20" }
+        };
+        assert.deepStrictEqual(parseCommand(userArgs), expectedValue);
+    });
+
+    it("should give parse command for date and empId query", function() {
+        const userArgs = ["--query", "--empId", 25275, "--date", "2019-11-20"];
+        const expectedValue = {
+            command: "query",
+            isValid: true,
+            value: { empId: 25275, date: "2019-11-20" }
         };
         assert.deepStrictEqual(parseCommand(userArgs), expectedValue);
     });
